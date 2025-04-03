baa83be991db83f38d5aa36024ab50e34f17ac23
Bruka25 <bkteshome862@gmail.com>
2025-04-03 21:04:01 +0300

    add a basic data models for employee and department

---
+++ hr-app/src/app/models/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  hireDate: Date;
+  departmentId: number;
+  position: string;
+  salary: number;
+}
